2f7459df75a22129ba09d0c8e9861617b72cb857
Yigit-Sert <yigitsert32@gmail.com>
2024-03-09 11:34:25 +0300

    New file system adopted due to the future needs foreseen.

--- navigation/AppNavigatior.js
+++ navigation/AppNavigatior.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from '../screens/LoginScreen';
-import MainScreen from '../screens/MainScreen';
-import SalesScreen from '../screens/SalesScreen';
-import PaymentScreen from '../screens/PaymentScreen';
-import ProductsScreen from '../screens/ProductsScreen';
-import SettingsScreen from '../screens/SettingsScreen';
-import ReportScreen from '../screens/ReportScreen';
+import LoginScreen from '../screens/Authentication/LoginScreen';
+import MainScreen from '../screens/Main/MainScreen';
+import SalesScreen from '../screens/Sales/SalesScreen';
+import PaymentScreen from '../screens/Payment/PaymentScreen';
+import ProductsScreen from '../screens/Products/ProductsScreen';
+import SettingsScreen from '../screens/Settings/SettingsScreen';
+import ReportScreen from '../screens/Reports/ReportScreen';
 
 const Stack = createNativeStackNavigator();
 
