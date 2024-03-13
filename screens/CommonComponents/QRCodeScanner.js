11ff85a96f6df8e216c7ea96ebccf9051369b220
Yigit-Sert <yigitsert32@gmail.com>
2024-03-13 17:20:10 +0300

    Add react-native-camera and react-native-qrcode-scanner dependencies

--- screens/CommonComponents/QRCodeScanner.js
+++ screens/CommonComponents/QRCodeScanner.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Alert } from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
+
 
 const HelloWorld = () => {
+    cons [qrCode, setQrCode] = useState('');
+
     return (
-        <View>
-            <Text>Hello World!</Text>
-        </View>
+        <QRCodeScanner
+            onRead={({ data }) => Setdata(data)}
+            // flashMode={RNCamera.Constants.FlashMode.torch}   // torch light on
+            reactivate={true}
+            reactivateTimeout={2000}
+            showMarker={true}
+            topContent={
+                <View>
+                    <Text>{qrCode}</Text>
+                </View>
+            }
+            bottomContent={
+                <View>
+                    <Text>Scan QR Code</Text>
+                </View>
+            }
+        />
     );
 };
 
